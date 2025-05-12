70e383b6b90d6d7229f1ec033c17d2253029cdc4
Aiden Zepp <aiden.zepp@superbuilders.school>
2025-05-12 11:20:41 -0500

    db

--- src/db/schemas/workflowy.ts
+++ src/db/schemas/workflowy.ts
@@ -1,23 +1,10 @@
-import { integer, json, pgSchema, text, timestamp, uuid } from "drizzle-orm/pg-core"
+import { pgSchema, text, timestamp } from "drizzle-orm/pg-core"
 
 export const workflowySchema = pgSchema("workflowy")
 
-export const metaData = workflowySchema.table("meta_data", {
+export const brainlift = workflowySchema.table("brainlift", {
   id: text("id").primaryKey(),
-  createdBy: integer("created_by").notNull(),
-  updatedBy: integer("updated_by").notNull(),
+  md: text("md").notNull(),
   createdAt: timestamp("created_at").notNull(),
   updatedAt: timestamp("updated_at").notNull(),
-})
-
-export const treeData = workflowySchema.table("tree_data", {
-  id: uuid("id").primaryKey(), // "id"
-  contents: text("contents").notNull(), // "nm"
-  priority: integer("priority").notNull(), // "pr"
-  parentId: uuid("parent_id"), // "prnt"
-  metadata: json("metadata").notNull().default("{}"), // "metadata"
-  createdBy: integer("created_by").notNull(), // "cb"
-  updatedBy: integer("updated_by").notNull(), // "lmb"
-  createdAt: timestamp("created_at").notNull(), // "ct"
-  updatedAt: timestamp("updated_at").notNull(), // "lmb"
 })
