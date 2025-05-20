50fde8691c84362d814312cf696ca05f796f8b22
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-05-20 13:50:43 -0500

    fix

--- src/errors.ts
+++ src/errors.ts
@@ -1,16 +1,16 @@
-export type Success<T> = {
+type Success<T> = {
 	data: T
 	error: undefined
 }
 
-export type Failure<E> = {
+type Failure<E> = {
 	data: undefined
 	error: E
 }
 
-export type Result<T, E = Error> = Success<T> | Failure<E>
+type Result<T, E = Error> = Success<T> | Failure<E>
 
-export async function tryCatch<T, E extends Error = Error>(
+async function tryCatch<T, E extends Error = Error>(
 	promise: Promise<T>
 ): Promise<Result<T, E>> {
 	try {
@@ -22,7 +22,7 @@ export async function tryCatch<T, E extends Error = Error>(
 	}
 }
 
-export function trySync<T, E extends Error = Error>(fn: () => T): Result<T, E> {
+function trySync<T, E extends Error = Error>(fn: () => T): Result<T, E> {
 	try {
 		const data = fn()
 		return { data, error: undefined }
@@ -32,7 +32,7 @@ export function trySync<T, E extends Error = Error>(fn: () => T): Result<T, E> {
 	}
 }
 
-export class WrappedError<E extends Error = Error> extends Error {
+class WrappedError<E extends Error = Error> extends Error {
 	cause: E
 
 	constructor(message: string, cause: E) {
@@ -66,10 +66,7 @@ export class WrappedError<E extends Error = Error> extends Error {
 	}
 }
 
-export function wrap<E extends Error>(
-	cause: E,
-	message: string
-): WrappedError<E> {
+function wrap<E extends Error>(cause: E, message: string): WrappedError<E> {
 	return new WrappedError(message, cause)
 }
 
